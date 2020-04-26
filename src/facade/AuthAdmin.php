382c9b18c97e9a355ffb65f51dd657491129fce1
zqs <zqscjj@163.com>
2020-04-26 22:11:28 +0800

    添加 facade注释，ide更友好

--- src/facade/AuthAdmin.php
+++ src/facade/AuthAdmin.php
@@ -3,6 +3,12 @@ namespace zqs\admin\facade;
 
 use think\Facade;
 
+/**
+ * Class AuthAdmin
+ * @package zqs\admin\facade
+ * @mixin \zqs\admin\lib\AuthAdmin
+ * @see \zqs\admin\lib\AuthAdmin
+ */
 class AuthAdmin extends Facade
 {
     protected static function getFacadeClass()
--- src/facade/Builder.php
+++ src/facade/Builder.php
@@ -3,6 +3,12 @@ namespace zqs\admin\facade;
 
 use think\Facade;
 
+/**
+ * Class Builder
+ * @package zqs\admin\facade
+ * @see \zqs\admin\builder\Builder
+ * @mixin \zqs\admin\builder\Builder
+ */
 class Builder extends Facade
 {
     protected static function getFacadeClass()
